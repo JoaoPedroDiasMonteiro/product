be75e77f14299d9f5a128463f5584553461645ba
João Pedro <joaopedro453@hotmail.com>
2021-06-05 20:24:13 -0300

    adicionando componente vue de paginação

---
+++ resources/js/Pages/Components/Pagination/Pagination.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col items-center px-5 py-5 bg-white border-t xs:flex-row xs:justify-between">
+    <span class="text-xs text-gray-900 xs:text-sm">
+        Showing {{paginate.from}} to {{paginate.to}} of {{paginate.total}} Entries
+    </span>
+    <div class="inline-flex mt-2 xs:mt-0">
+      <inertia-link v-if="paginate.prev_page_url" :href="paginate.prev_page_url" class="px-4 py-2 text-sm font-semibold text-gray-800 bg-gray-300 rounded-l hover:bg-gray-400">
+        Prev
+      </inertia-link>
+      <inertia-link v-if="paginate.next_page_url" :href="paginate.next_page_url" class="px-4 py-2 text-sm font-semibold text-gray-800 bg-gray-300 rounded-r hover:bg-gray-400">
+        Next
+      </inertia-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    paginate: Object
+  }
+}
+</script>
+
+<style></style>
