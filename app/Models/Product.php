109d3b69e53024c08922f5b256bc0e12f7a6893d
João Pedro <joaopedro453@hotmail.com>
2021-06-07 20:34:22 -0300

    add Product migration e Model

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\PriceFormat;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory, PriceFormat;
+
+    protected $fillable = [
+        'name',
+        'cost_price',
+        'sell_price',
+        'description'
+    ];
+
+    public function setCostPriceAttribute($value)
+    {
+        $this->attributes['cost_price'] = $this->toFloatNumber($value);
+    }
+
+    public function getCostPriceAttribute($value)
+    {
+        return $this->toBrl($value);
+    }
+
+    public function setSellPriceAttribute($value)
+    {
+        $this->attributes['sell_price'] = $this->toFloatNumber($value);
+    }
+
+    public function getSellPriceAttribute($value)
+    {
+        return $this->toBrl($value);
+    }
+}
