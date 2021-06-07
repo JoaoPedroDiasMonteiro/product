b7f312c6b96205340d30f2af09049cd2ce5f7a97
João Pedro <joaopedro453@hotmail.com>
2021-06-06 21:08:26 -0300

    add MainNavBar component em List.vue

--- resources/js/Pages/Users/List.vue
+++ resources/js/Pages/Users/List.vue
@@ -1,11 +1,14 @@
 <template>
-  <table-menu :routes="routes"></table-menu>
-  <simple-with-actions
-    :data="data"
-    :rows="rows"
-    :routes="routes2"
-  ></simple-with-actions>
-  <pagination :paginate="data"></pagination>
+  <div class="m-3">
+    <main-nav-bar class="mb-6" :routes="routes" :searchProp="searchProp">
+    </main-nav-bar>
+    <simple-with-actions
+      :data="data"
+      :rows="rows"
+      :routes="routes2"
+    ></simple-with-actions>
+    <pagination :paginate="data"></pagination>
+  </div>
 </template>
 
 <script>
@@ -13,12 +16,20 @@ import TableMenu from "../../Components/Menus/TableMenu.vue";
 import Pagination from "../../Components/Pagination/Pagination.vue";
 import SimpleWithActions from "../../Components/Tables/SimpleWithActions.vue";
 import MasterLayout from "../../Components/Layouts/MasterLayout.vue";
+import MainNavBar from "../../Components/Menus/MainNavBar.vue";
 
 export default {
-  layout: (h, page) => h(MasterLayout, [page]),
-  components: { SimpleWithActions, Pagination, TableMenu, MasterLayout },
+  layout: MasterLayout,
+  components: {
+    SimpleWithActions,
+    Pagination,
+    TableMenu,
+    MasterLayout,
+    MainNavBar
+  },
   props: {
-    data: Object
+    data: Object,
+    searchProp: String
   },
   data() {
     return {
