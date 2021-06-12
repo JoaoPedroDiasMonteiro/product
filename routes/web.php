489a8876c962d6448b0cf9503459abe9ef95dd48
João Pedro <joaopedro453@hotmail.com>
2021-06-11 23:58:09 -0300

    update web routes, add rotas de sale order

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleOrderController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -31,6 +32,7 @@ Route::get('/test', [TestController::class, 'test'])->name('test');
 Route::resource('users', UserController::class);
 Route::resource('products', ProductController::class);
 Route::resource('customers', CustomerController::class);
+Route::resource('sale-orders', SaleOrderController::class);
 
 
 Route::get('/dashboard', [LoginController::class, 'index'])->name('admin.dashboard');
