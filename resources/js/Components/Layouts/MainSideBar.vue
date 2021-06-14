fa6da68ba8825a2d2a11102f0a3a0bb85a4e0cc8
João Pedro <joaopedro453@hotmail.com>
2021-06-14 00:50:42 -0300

    update MainSideBar.vue, adicionando link para Sale Orders

--- resources/js/Components/Layouts/MainSideBar.vue
+++ resources/js/Components/Layouts/MainSideBar.vue
@@ -83,6 +83,22 @@
               </span>
               <span class="ml-2">Customers</span>
             </inertia-link>
+
+            <inertia-link
+              :class="{ active: isRoute('sale-orders.') }"
+              :href="route('sale-orders.index')"
+              class="flex flex-row px-4 py-4 mb-2 text-gray-100 border-gray-300 rounded-lg hover:text-black hover:bg-white hover:font-bold hover:cursor-pointer"
+            >
+              <span>
+               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
+                </svg>
+              </span>
+              <span class="ml-2">Sale Orders</span>
+            </inertia-link>
+
+
           </li>
         </ul>
       </div>
