ed7573781953bac73fea1e845db03882490e0b88
João Pedro <joaopedro453@hotmail.com>
2021-06-14 23:10:03 -0300

    fix AdminRequired Middleware, tentar arrumar bug do servidor

--- app/Http/Middleware/AdminRequired.php
+++ app/Http/Middleware/AdminRequired.php
@@ -18,7 +18,7 @@ class AdminRequired
     public function handle(Request $request, Closure $next)
     {
         $user = Auth::user();
-        if ($user && $user->is_admin) {
+        if ($user && $user->is_admin === 1) {
             return $next($request);
         }
 
